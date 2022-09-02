1ff84fa571196d78339cf7523f77d83429c9e658
Kenya Ishizuka <ken8116@icloud.com>
2022-09-02 22:50:12 +0900

    TOPページの表示のテスト

---
+++ tests/Feature/Http/Controllers/PostListControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostListControllerTest extends TestCase
+{
+    /** @test */
+    public function TOPページでブログ一覧が表示される()
+    {
+        $this->get('/')
+            ->assertOk();
+    }
+}
