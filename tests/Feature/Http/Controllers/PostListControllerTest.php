39388ec51cb410c082339aa05073f5a0211b4344
Kenya Ishizuka <ken8116@icloud.com>
2022-09-04 23:41:10 +0900

    test detabase

--- tests/Feature/Http/Controllers/PostListControllerTest.php
+++ tests/Feature/Http/Controllers/PostListControllerTest.php
@@ -5,13 +5,23 @@ namespace Tests\Feature\Http\Controllers;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Post;
+
 
 class PostListControllerTest extends TestCase
 {
+    use RefreshDatabase;
     /** @test */
     public function TOPページでブログ一覧が表示される()
     {
+        $post1 = Post::factory()->create();
+        $post2 = Post::factory()->create();
+
+
+
         $this->get('/')
-            ->assertOk();
+            ->assertOk()
+            ->assertSee($post1->title)
+            ->assertSee($post2->title);
     }
 }
